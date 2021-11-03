7a9ecf393389813597be495b01e1f1ad93e52a33
Peprah94 <31469850+Peprah94@users.noreply.github.com>
2021-11-03 14:27:57 +0100

    Update lin_reg.R
    Changes in the code

--- lin_reg.R
+++ lin_reg.R
@@ -8,7 +8,7 @@ library(coda)
 
 # function for generating samples
 sample.linreg <- function(){
-  n = 100
+  n = 100 # Number of samples
   x1 = runif(n) #covariate 1
   x2 = runif(n) #covariate 2
   err = rnorm(n) # error
@@ -19,85 +19,86 @@ sample.linreg <- function(){
 # Function that estimates beta0 and precision
 #We return the mean, although we need the marginals
 #We have to sort that out later
-
 fit.inla <- function(x, y, beta){
   data <- list(y=y, x=x)
   data$oset = data$x %*% (beta)
-  res = INLA::inla(y ~ 1 + offset(oset), data = data)
+  res = INLA::inla(y ~ 1 + offset(oset), data = data) #Fitting INLA model with fixed beta1, beta2
   intercept = INLA::inla.emarginal(function(x) x,res$marginals.fixed[[1]])
   precision = INLA::inla.emarginal(function(x) x,res$marginals.hyper[[1]])
   ret <- c(intercept, precision)
   return(ret)
-  #list(#mlik = res$mlik,
- #dists = list(intercept = res$marginals.fixed[[1]],
- #           precision = res$marginals.hyperpar[[1]])))
 }
 
-#Testing the function fit.inla
-beta <- (c(0,0))
+##Testing the function fit.inla
+df <- sample.linreg() #Data from the sampling fnx
+beta <- (c(2,-2)) #Fixed beta1, beta2
 fit.inla(df$x,df$y, beta)
 
-## Make the INLA fnx compatible with NIMBLE
-nimbleINLA<- nimbleRcall(function(x=double(2), y=double(1), beta=double(1)){}, Rfun="fit.inla", 
-                                returnType = double(1))
+
+## Make the INLA function compatible with NIMBLE
+nimble_INLA <- nimbleRcall(
+  prototype = function(
+    x=double(2), #x is a matrix 
+    y=double(1), #y is a vector
+    beta=double(1) # beta is a vector
+  ) {},
+  returnType = double(1), # outcome is a vector
+  Rfun = 'fit.inla'
+)
+
 
 #Compiling the nimble function
-CnimbleINLA <- compileNimble(nimbleINLA)
+CnimbleINLA <- compileNimble(nimble_INLA)
 
 #Testing the compiled function. 
 #Should give the same results as fit.inla
 CnimbleINLA(df$x,df$y, beta)
 
-#Alternative writting of nimble function
-#This fails to use INLA
-nimINLA <- nimbleFunction(
-  run=function(x=double(2),y=double(1), beta=double(1)){
-    library(INLA)
-    data <- nimbleList(y=y, x=x) 
-    data$oset = data$x %*% (beta)
-    res = inla(y ~ 1 + offset(oset), data = data)
-    intercept = inla.emarginal(function(x) x,res$marginals.fixed[[1]])
-    precision = inla.emarginal(function(x) x,res$marginals.hyper[[1]])
-    ans <- nimC(intercept, precision)
-    return(ans)
-    returnType(double(1))
-  }
-)
+#Simple code to test parameters
+code <-nimbleCode({
+  inla.res[1:2] <- nimble_INLA(x_obs[1:N,1:2],y_obs[1:N],beta[1:2])
+  #for(j in 1:2){
+    beta[1] ~ dnorm(0,sd=1)  
+    beta[2] ~ dnorm(0,sd=1)
+  #}
+
+})
 
 #Code for MCMC in NIMBLE
-code <- nimbleCode({
+#code <- nimbleCode({
   #Prior for beta1 and beta2
-  for(j in 1:2){
-    beta[j] ~ dnorm(0,5)  
-  }
+  #for(j in 1:2){
+ #   beta[j] ~ dnorm(0,sd = 5)  
+ # }
 
   #Bivariate linear model specification
-  for(i in 1:N){
-    linpred[i] <- inla.res[1]+ beta[1]*x[i,1]+ beta[2]*x[i,2]
-    y[i] ~ dnorm(linpred[i],inla.res[2]) 
+  #for(i in 1:N){
+    #linpred[i] <- inla.res[1]+ beta[1]*x[i,1]+ beta[2]*x[i,2]
+  #  linpred[i] <- inla.res[1]+ 1*x[i,1]+ 2*x[i,2]
+  #  y_obs[i] ~ dnorm(linpred[i],inla.res[2]) 
     
     #We test with constant values for constant and the model works
     #linpred[i] <- 1+ beta[1]*x[i,1]+ beta[2]*x[i,2]
     #y[i] ~ dnorm(linpred[i],sd=1 ) 
 
-  }
+ # }
   
   #Fitting the inla with the simulated parameters
-  inla.res[1:2] <- CnimbleINLA(x[1:N,1:2],y[1:N],beta[1:2])
-})
+  #inla.res[1:2] <- CnimbleINLA(x[1:N,1:2],y[1:N],beta[1:2])
+#})
 
 ## Parameterising the nimble model
 
 #Data
-inla_data <- list(y=df$y, 
-              x = df$x)
+inla_data <- list(y_obs=df$y, 
+                  x_obs = df$x)
 
 #Constants
-const <- list(N = length(df$y))
+const <- list(N = 100)
 
 # Initial values
-idm_inits <- function(){list(beta =c(0,0),
-                             inla.res =c(1,0.7)
+idm_inits <- function(){list(beta=c(1,1)
+                             
 )
 }
 
@@ -107,20 +108,22 @@ initsList <- idm_inits()
 modelInfo <- list(
   code = code,
  constants = const,
-  data = inla_data,
-  inits = initsList
-)
+  data = inla_data)#,
+ # inits = initsList
+#)
 
 #Create the model in nimble
-mwtc <- nimbleModel(code,data = inla_data, 
-                    constants = const, 
-                    inits = initsList)
+mwtc <- nimbleModel(code, 
+                    data = inla_data,
+                    constants = const#, 
+                    #inits = initsList
+                    )
 
 # Create the model in C
 Cmwtc <- compileNimble(mwtc,showCompilerOutput = FALSE) #Have issues compiling
 
 
-mcmcconf <- configureMCMC(Cmwtc, monitors = c("beta", "inla.res"))
+mcmcconf <- configureMCMC(Cmwtc, monitors = c("beta"))#, "inla.res"))
 
 Rmcmc <- buildMCMC(mcmcconf, 
                    enableWAIC =FALSE)
@@ -132,9 +135,9 @@ cmcmc <- compileNimble(Rmcmc,
 
 # Run the MCMC
 mcmc.out <- runMCMC(cmcmc, 
-                    niter = 50000,
+                    niter = 5000,
                     nchains = 3,
-                    nburnin = 25000,
+                    nburnin = 2500,
                     #inits = initsList,
                     #thin =10, 
                     setSeed = TRUE, 
